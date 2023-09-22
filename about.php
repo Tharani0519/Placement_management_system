378ed7094b3c94bce6fad8258236700e5534e306
Tharani0519 <142311766+Tharani0519@users.noreply.github.com>
2023-09-23 00:05:59 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,252 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+<title>Drives :: About</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tillage Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstarp-css -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--// bootstarp-css -->
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--// css -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!--fonts-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,800,700,600' rel='stylesheet' type='text/css'>
+<!--/fonts-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+	 new WOW().init();
+</script>
+</head>
+<body>
+	<!-- banner -->
+	<div class="banner a-banner">
+		<!-- container -->
+		<div class="container">
+			<div class="header">
+				<div class="head-logo">
+					
+				</div>
+				<div class="top-nav">
+					<span class="menu"><img src="images/menu.png" alt=""></span>
+					<ul class="nav1">
+						<li class="hvr-sweep-to-bottom">
+							<!--	<a href="../Homepage/index.php">CUSAT-SOE<i>Goto Placement Homepage</i></a> -->
+							</li>
+						<li class="hvr-sweep-to-bottom">
+								<a href="index.php">Home<i> Overview of the Site</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom active">
+								<a href="about.php">About<i>About us & our excellence in placements</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom">
+								<a href="products.php">Placement Hub<i>Campus Drives</i></a>
+							</li>
+							
+							<li class="hvr-sweep-to-bottom">
+								<a href="mail.php">Mail Us<i>Get in Touch with us</i></a>
+							</li> 
+
+							
+						<!--	<li class="hvr-sweep-to-bottom active">
+								<a href="index.php">Home<i>Overview of the Site</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom">
+								<a href="about.php">About<i>About Us & our excellence in placements</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom">
+								<a href="../products.php">Placement hub<i>Campus Drives</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom">
+								<a href="mail.php">Mail Us<i>Get in Touch with us</i></a>
+							</li>
+-->
+							<div class="clearfix"></div>
+						<div class="clearfix"> </div>
+					</ul>
+					<!-- script-for-menu -->
+							 <script>
+							   $( "span.menu" ).click(function() {
+								 $( "ul.nav1" ).slideToggle( 300, function() {
+								 // Animation complete.
+								  });
+								 });
+							</script>
+						<!-- /script-for-menu -->
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //banner -->
+	<!-- about -->
+	<div class="about-top">
+		<!-- container -->
+		<div class="container">
+			<div class="about-info wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+				<h3><b>A few words about us</b></h3>
+				
+			</div>
+			<div class="banner-bottom-grids a-banner-bottom-grids">
+				<div class="col-md-5 banner-bottom-right wow fadeInRight animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<img src="images/pexels-rdne-stock-project-7713371.jpg" alt="">
+				</div>
+				<div class="col-md-7 a-banner-bottom-text">
+					<div class="jumbotron banner-bottom-left wow fadeInLeft animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					  <h3> </h3>
+						<h5>Students have access to a comprehensive range of resources that can help them secure suitable placements. Our system help students create strong profiles that showcase their qualifications, access job opportunities,  and support throughout the placement process. Ultimately, placement management systems are an excellent resource for students seeking to maximize their chances of securing their dream placements.</span></h5>
+						<p> </p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //about -->
+	<!-- about-bottom -->
+	<div class="about-bottom">
+		<!-- container -->
+	<!--	<div class="container">
+			<h3 class="wow fadeInRight animated" style="visibility: visible;">To be typeddd</h3>
+			<div class="about-bottom-grids">
+				<div class="col-md-6 about-bottom-left wow fadeInLeft animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<p>Want to be filled</p>
+				</div>
+				<div class="col-md-6 about-bottom-left about-bottom-right wow fadeInRight animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<p>Yet to be filled</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div> -->
+		<!-- //container -->
+	</div> 
+							
+	<!-- //about-bottom -->
+	<!-- team -->
+	<div class="team">
+		<!-- container -->
+		<div class="container">
+			<div class="team-info">
+				<h3 class="wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">Remarkable ACHIEVEMENTS</h3>
+				<div class="team-grids">
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/IMG_20230517_083730.jpg"  alt="">
+						<h6>MuraliKrishnan</h6>
+						<p>Software Developer </p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/IMG_20230517_083720.jpg" alt="">
+						<h6>Saran kumaran</h6>
+						<p>Web Developer</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b3.jpg" alt="">
+						<h6> Varthini</h6>
+						<p>Full Stack Developer</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b4.jpg" alt="">
+						<h6>Naveen Raj</h6>
+						<p>System Engineer</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //team -->
+	<!-- footer -->
+		<div class="footer">
+			<!-- container -->
+			<div class="container">
+				<div class="col-md-6 footer-left  wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+						<li>
+							<a href="index.php">Home</a>
+						</li>
+						<li>
+							<a href="about.php">About</a>
+						</li>
+						<li>
+							<a href="products.php">Campus Drives</a>
+						</li>
+						<li>
+							<a href="../Gallery/index.html">Gallery</a>
+						</li>
+						<li>
+							<a href="mail.php">Mail Us</a>
+						</li>
+					</ul>
+					<form>
+						<input type="text" placeholder="Email" required="">
+						<input type="submit" value="Subscribe">
+					</form>
+				</div>
+				<div class="col-md-3 footer-middle wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<h3>Address</h3>
+					<div class="address">
+						<p>College of Engineering,Guindy
+							<span>Chennai,Tamil Nadu</span>
+						</p>
+					</div>
+					<div class="phone">
+						<p>044 - 2235 7080 / 81</p>
+					</div>
+				</div>
+				<div class="col-md-3 footer-right  wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					
+					
+			
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<!-- //container -->
+		</div>
+		<!-- //footer -->
+		<div class="copyright">
+			<!-- container -->
+			<div class="container">
+				<div class="copyright-left wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					 <p>Copyright &copy; AU CEG | Developed by Innovative minds of CEG
+              <a href="http://znumerique.azurewebsites.net" target="_parent"></a>
+					</p>
+				</div>
+				<div class="copyright-right wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+						<li>
+							<a href="#" class="twitter"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter facebook"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter chrome"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter pinterest"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter linkedin"> </a>
+						</li>
+						<li>
+							<a href="#" class="twitter dribbble"> </a>
+						</li>
+					</ul>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<!-- //container -->
+		</div>
+</body>
+</html>
